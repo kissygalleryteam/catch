4d52d4f72e6d4c1b79414aa1dd3d7ded12ecb946
wuming.xiaowm <wuming.xiaowm@taobao.com>
2014-03-06 22:58:33 +0800

    add file

---
+++ 1.0/demo.js
@@ -0,0 +1,418 @@
+(function(S){
+	var $;
+	var clickType = 'click';
+
+	S.config({
+        packages: [{
+            name: 'tbc',
+            path: 'http://g.tbcdn.cn/tbc/',
+            ignorePackageNameInUri: true
+        }]
+    });
+
+	S.use('node,event,anim',function(S){
+		$ = S.all;
+		game.init();
+	});
+
+	/**
+	 * 游戏
+	 * @type {Object}
+	 */
+	var game = {
+		/**
+		 * 初始化
+		 */
+		init : function(){
+			var self = this;
+
+			self._bindClose();
+			
+			self.loadCatchLib(function(_Catch){
+				self.initGameLib(_Catch);
+				self.play();
+			});
+
+			self.reople(0,100);
+
+		},
+		_startStatus : false,
+		/**
+		 * 欢迎页面的动画
+		 */
+		reople : function(i,time){
+			var self = this;
+			document.getElementById('J_reople').className = 'reople reople'+i;
+			setTimeout(function(){
+				i++;
+				if(i > 6){
+					i = 0;
+				}
+				if(i == 4){
+					time = 800;
+				}else{
+					time = 100;
+				}
+				if(!self._startStatus){
+					self.reople(i,time);
+				}
+			},time);
+		},
+		/**
+		 * 开始文档
+		 */
+		play : function(){
+			var self = this,
+				playBtn = $('#J_play');
+
+			playBtn.on(clickType,function(e){
+				e.preventDefault();
+				var btnParent = playBtn.parent();
+				btnParent.addClass('btnRotate');
+
+				self._startStatus = true;
+
+				setTimeout(function(){
+					btnParent.animate({top:'180px'},{'queue':'game','duration':0.1});
+					btnParent.animate({top:'500px'},{'queue':'game','duration':0.2,'complete':function(){
+						btnParent.remove();
+					}});
+					self.hideCloseBtn();
+				},20);
+				setTimeout(function(){					
+					$('#J_welcome_up').animate({top:"-350px"},{'duration':0.3});
+					$('#J_welcome_down').css('background-color','transparent').animate({top:"600px"},{'duration':0.3,'complete':function(){
+						self.startGame();
+						$('#J_welcome').hide();
+					}});
+					$('#J_reople').animate({top:"600px"},{'duration':0.3});
+				},500);
+			});
+		},
+		_closeBtn : null,
+		_bindClose : function(){
+			var self = this;
+			self._closeBtn = $('#J_close_btn');
+			self._closeBtn.on(clickType,function(e){
+				e.preventDefault();
+				self.closeGame();
+			});
+		},
+		hideCloseBtn : function(){
+			this._closeBtn.hide();
+		},
+		showCloseBtn : function(){
+			this._closeBtn.show();
+		},
+		/**
+		 * 加载组件
+		 * @param  {Function} callback [description]
+		 */
+		loadCatchLib : function(callback){
+			 var self = this;
+			 var srcPath = "./../1.0";
+			 S.Config.debug = true;
+	         S.config({
+	            packages:[
+	                {
+	                    name:"index",
+	                    path:srcPath,
+	                    charset:"utf-8",
+	                    ignorePackageNameInUri:true
+	                }
+	            ]
+	        });
+
+	        S.use('index', function (S, Catch) {
+	        	callback(Catch);
+	        });
+		},
+		_Catch : null,
+		/**
+		 * 初始化游戏
+		 */
+		initGameLib : function(_Catch){
+			var self = this;
+			self._Catch = new _Catch('#J_game_main',{
+				'fall' : [
+							{
+		                        'className' : 'redHeart',
+		                        'size':1000,
+		                        'width' : 94,
+		                        'height' : 102,
+		                        'speed' : 20,
+                        		'time':30,
+		                        'type':2
+		                    },
+		                    {
+		                        'className':'yellowHeart',
+		                        'size':1000,
+		                        'width' : 94,
+		                        'height' : 102,
+		                        'speed' : 20,
+                        		'time':30,
+		                        'type':1
+		                    },
+		                    {
+		                        'className' : 'phone',
+		                        'size':500,
+		                        'width' : 94,
+		                        'height' : 102,
+		                        'speed' : 20,
+                        		'time':30,
+		                        'type':3
+		                    },
+		                    {
+		                        'className' : 'pad',
+		                        'size':500,
+		                        'width' : 94,
+		                        'height' : 102,
+		                        'speed' : 20,
+                        		'time':30,
+		                        'type':4
+		                    },
+		                    {
+		                        'className' : 'computer',
+		                        'size':500,
+		                        'width' : 94,
+		                        'height' : 102,
+		                        'speed' : 20,
+                        		'time':30,
+		                        'type':5
+		                    }
+						 ],
+				 'main':{
+		                    className : 'bed',
+		                    width:122,
+		                    height:175,
+		                    direc:'free'
+		                },
+		          'maxWidth' : 990,
+	              'maxHeight' : 550,
+	              'time':1800000,
+	              'speedUp':[
+	              			{
+	              				delay:10000,
+	              				times:1.5
+	              			},
+	              			{
+	              				delay:15000,
+	              				times:2
+	              			}
+	              		  ],
+	              itemCallback : self.itemCallback,
+	              completeCallback : self.completeCallback
+			});
+
+		},
+		/**
+		 * 启动游戏
+		 */
+		startGame : function(){
+			var self = this;
+			self._Catch.start();
+			$('#J_game_main').addClass('start');
+		},
+		/**
+		 * 游戏完成
+		 * @return {[type]} [description]
+		 */
+		completeCallback:function(){
+			game.gemeStop();
+		},
+		/**
+		 * 每次吃到物体回掉
+		 */
+		itemCallback : function(mainBox, type, left, top){
+			var self = game;
+			if(type == 1 || type == 2){
+				setTimeout(function(){
+					mainBox.addClass('bed-happy');
+				},100);
+				setTimeout(function(){
+					mainBox.removeClass('bed-happy').addClass('bed-normal');
+				},200);
+				setTimeout(function(){
+					mainBox.removeClass('bed-normal').addClass('bed-happy');
+				},300);
+				setTimeout(function(){
+					mainBox.removeClass('bed-happy').addClass('bed-normal');
+				},400);
+				setTimeout(function(){
+					mainBox.removeClass('bed-normal');
+				},500);
+
+				self.createPlus(type,left,top);
+			}else{
+				mainBox.addClass('bed-sad');
+				self._Catch.stop();
+				
+				setTimeout(function(){
+					self.gemeStop();
+					mainBox.hide();
+				},800);
+			}
+		},
+		//分数
+		_scores : 0,
+		/**
+		 * 创建加分
+		 */
+		createPlus : function(type,left,top){
+			var self = this;
+			var dom = $('<div></div>'),
+				gameMain = $('#J_game_main');
+			dom.addClass('plus'+type);
+			dom.css({
+				'left' : left,
+				'top' : top
+			});
+			gameMain.append(dom);
+
+			dom.animate({
+				top:(top - 100)+'px'
+			},{'duration':0.3,'complete':function(){
+				dom.remove();
+			}});
+
+			switch(type){
+				case 1 : 
+					self._scores += 10;
+					break;
+				case 2 : 
+					self._scores += 20;
+					break;
+			}
+
+			$('#J_hour').text(parseInt(self._scores/60));
+			$('#J_minute').text(self._scores%60);
+			
+		},
+		/**
+		 * 游戏已经停止了
+		 */
+		gemeStop : function(){
+			var self = this,
+				scores = self._scores,
+				className = 'bg5',
+				shareType = 5,
+				shareText = '我在睡眠日游戏中才睡了0小时，好困哦~';
+			var hour = parseInt(self._scores/60),
+				minute = self._scores%60;
+			if(scores >= 480){
+				className = 'bg1';
+				shareType = 1;
+				shareText = '我在睡眠日游戏中睡了'+hour+'小时'+minute+'分，颤抖吧，地球人！';
+			}else if(scores >= 360 && scores < 480){
+				className = 'bg2';
+				shareType = 2;
+				shareText = '我在睡眠日游戏中睡了'+hour+'小时'+minute+'分，腰不酸，痛不痛，干活也有劲了！';
+			}else if(scores >= 240 && scores < 360){
+				className = 'bg3';
+				shareType = 3;
+				shareText = '我在睡眠日游戏中睡了'+hour+'小时'+minute+'分，咦，为毛赶脚飘了起来？';
+			}else if(scores >=120 && scores < 240){
+				className = 'bg4';
+				shareType = 4;
+				shareText = '我在睡眠日游戏中睡了'+hour+'小时'+minute+'分，最近数星星一直数到天亮！';
+			}else{
+				className = 'bg5';
+				shareType = 5;
+				shareText = '我在睡眠日游戏中睡了'+hour+'小时'+minute+'分。好困哦~';
+			}
+
+			var dom = $('<div class="result" id="J_result">\
+			                <span class="hour">'+parseInt(self._scores/60)+'</span>\
+			                <span class="minute">'+(self._scores%60)+'</span>\
+			                <button class="close-btn" type="button"></button>\
+			                <button class="restart-btn" type="button"></button>\
+			                <button class="share-btn" type="button"></button>\
+			            </div>');
+			dom.addClass(className);
+			$('#J_game_main').append(dom);
+
+			dom.one('.restart-btn').on(clickType,function(e){
+				e.preventDefault();
+				self.restatusGame();
+			});
+
+			dom.one('.close-btn').on(clickType, function(e){
+				e.preventDefault();
+				self.closeGame();
+			});
+
+			dom.one('.share-btn').on(clickType,function(e){
+				e.preventDefault();
+				self.share(shareType, shareText);
+			});
+
+			self.hideResultTime();
+
+			
+		},
+		/**
+		 * 重新开始游戏
+		 */
+		restatusGame : function(){
+			var self = this;
+			self.resetResultTime();
+			self._scores = 0;
+			$('#J_result').remove();
+			self._startStatus = true;
+			
+			//调用组件，重新开始游戏
+			self._Catch.restart();
+		},
+		/**
+		 * 隐藏结果时间
+		 */
+		hideResultTime : function(){
+			$('#J_result_time').hide();
+		},
+		/**
+		 * 重置结果时间
+		 */
+		resetResultTime : function(){
+			$('#J_result_time').show();
+			$('#J_hour').text(0);
+			$('#J_minute').text(0);
+		},
+		/**
+		 * 分享
+		 */
+		share : function(type,text){
+			var imgs = ['http://gtms01.alicdn.com/tps/i1/T1ZfO5FxXXXXcUdm2I-400-400.png','http://gtms04.alicdn.com/tps/i4/T1TQq3FCxaXXcUdm2I-400-400.png','http://gtms04.alicdn.com/tps/i4/T1TQq3FCxaXXcUdm2I-400-400.png','http://gtms03.alicdn.com/tps/i3/T1ulO4FCxXXXcUdm2I-400-400.png','http://gtms01.alicdn.com/tps/i1/T17ze1FwJcXXcUdm2I-400-400.png'];
+			var coverDom = $('<div class="cover"></div>');
+			$('body').append(coverDom);
+			S.use('tbc/share/1.0.2/', function(S, Share) {
+                Share.init({
+                    type : 'webpage',
+                    key : 'http://www.taobao.com',
+                    client_id : '180919',
+                    title : '321睡眠日保证每天睡够8小时，睡前远离手机！',
+                    thumb:imgs[type-1],
+                    comment:text,
+                    callback : {
+                    	success : function(){
+                    		coverDom.remove();
+                    	},
+                    	destory : function(e){
+                    		if(e.className.indexOf('J_TBC_Share')>-1){
+                    			coverDom.remove();	
+                    		}
+                    	}
+                    }
+                });
+                
+            });
+		},
+		/**
+		 * 关闭
+		 */
+		closeGame : function(){
+			console.log('关闭'); 
+		}
+	};
+
+
+})(KISSY);
--- 1.0/index.js
+++ 1.0/index.js
@@ -22,6 +22,8 @@ KISSY.add(function (S, Node,Base) {
     }
     
 	Catch.prototype = {
+        //游戏加速
+        _gameSpeedUp : 1,
         _maxHeight : 550,
         _maxWidth : 990,
         Jnode : null,
@@ -40,23 +42,66 @@ KISSY.add(function (S, Node,Base) {
             //设置最大值
             self._setMaxValue();
 
-            //创建主体
-            self._createMainBox(self.getMainCfg());    
-
-            //定时扫描接触点
-            self.setInterval = setInterval(function(){
-                self._parseCollide();
-            },30); 
-
-            // setTimeout(function(){
-            //     clearInterval(self.setInterval);
-            // },10000)
-
-            
-            self._setFall();
 		},
         /**
-         * [setFall description]
+         * 开始游戏
+         */
+        start : function(){
+            var self = this;
+            //设置状态
+            self._runStatus = true;
+            //开启任务定时跑
+            self['setInterval']();
+
+            //创建主体
+            self._createMainBox(self.getMainCfg());    
+            //创建下坠物体
+            self._setFall();
+
+            //加速
+            self._speedUp();
+        },
+        _runStatus : false,
+        /**
+         * 停止游戏
+         */
+        stop : function(){
+            var self = this;
+            self._runStatus = false;
+
+            //停止加速
+            self._stopSpeedUp();
+        },
+        /**
+         * 重新开始游戏
+         */
+        restart : function(){
+            var self = this;
+            //设置状态
+            self._runStatus = true;
+            //开启任务定时跑
+            self['setInterval']();
+            //显示主体
+            self._mainBox.show();
+
+            self._mainBox[0].className = self.cfg.main.className;
+
+            //下坠物体
+            for(var i in self._fallObj){
+                var item = self._fallObj[i];
+                item.dom.remove();
+                delete self._fallObj[i];
+            }
+
+            //创建下坠物体
+            self._setFall();
+            //重置速度
+            self._gameSpeedUp = 1;
+            //游戏加速
+            self._speedUp();
+        },
+        /**
+         * 创建下坠物体
          */
         _setFall : function(){
             var self = this;
@@ -78,8 +123,6 @@ KISSY.add(function (S, Node,Base) {
 
             var timeout = (self.cfg.time == undf ? 60000 : self.cfg.time) / len;
 
-            create(0);
-            
             function create(i){
                 var item = obj[indexs[i]],
                     width = item.width == undf ? 10 : item.width,
@@ -87,7 +130,8 @@ KISSY.add(function (S, Node,Base) {
                     left = item.left == undf ? self.getRandomNub(self.getArrData(0,self._maxWidth),1)[0] : item.left,
                     speed = item.speed == undf ? self.getRandomNub(self.getArrData(0,10),1)[0] : item.speed,
                     time = item.time == undf ? self.getRandomNub(self.getArrData(0,50),1)[0] : item.time,
-                    className = item.className == undf ? 'fall' : item.className;
+                    className = item.className == undf ? 'fall' : item.className,
+                    type = item.type == undf ? className : item.type
                 
                 //如果left大于画布宽度，那么left -10
                 if(left+width > self._maxWidth){
@@ -100,15 +144,40 @@ KISSY.add(function (S, Node,Base) {
                     left : left,
                     speed : speed,
                     time : time,
-                    className:className
+                    className:className,
+                    type : type
                 });
 
-                if(i == len){
+               
+
+                if(i == len || self._runStatus === false){
                     return;
                 }
+
                 setTimeout(function(){
-                    create(++i);
+                    i++
+                    if(i == len){
+                        setTimeout(function(){
+                            self._complete();
+                        },1000);
+                    }else{
+                        create(i);
+                    }
+                    
                 },timeout);
+            }
+
+            create(0);
+        },
+        /**
+         * 游戏完成
+         */
+        _complete : function(){
+            this['clearInterval']();
+            this.stop();
+            this._mainBox.hide();
+            if(this.cfg.completeCallback){
+                this.cfg.completeCallback();
             }
         },
         /**
@@ -143,7 +212,7 @@ KISSY.add(function (S, Node,Base) {
          */
         _checkCfg : function(){
             if(this.cfg == undf || this.cfg['main'] == undf){
-                alert('配置不全！')
+                alert('配置不全！');
             }
             return true;
         },
@@ -181,7 +250,8 @@ KISSY.add(function (S, Node,Base) {
             self._fallObj[self._i] = {
                 dom:dom,
                 top:cfg.top,
-                left:cfg.left
+                left:cfg.left,
+                type:cfg.type
             };
             
             self.Jnode.append(dom);
@@ -198,7 +268,11 @@ KISSY.add(function (S, Node,Base) {
                 return;
             }
 
-            cfg.top += cfg.speed;
+            if(self._runStatus === false){
+                return;
+            }
+
+            cfg.top += cfg.speed * self._gameSpeedUp;
 
             dom.css('top',cfg.top);
 
@@ -206,6 +280,7 @@ KISSY.add(function (S, Node,Base) {
             self._fallObj[i]['left'] = cfg.left;
 
             if(cfg.top > self._maxHeight){
+                self._fallObj[i].dom.remove();
                 delete self._fallObj[i];
                 return;
             }
@@ -213,6 +288,37 @@ KISSY.add(function (S, Node,Base) {
             setTimeout(function(){
                 self._fallMove(dom, cfg, i);
             },cfg.time);
+        },
+        _speedUpMap : [],
+        /**
+         * 游戏加速
+         */
+        _speedUp : function(){
+            var self = this;
+            if(self.cfg.speedUp == undf){
+                return;
+            }
+            self._speedUpMap = [];
+
+            for(var i=0;i<self.cfg.speedUp.length;i++){
+                var item = self.cfg.speedUp[i];
+                (function(item){
+                    var a = setTimeout(function(){
+                        self._gameSpeedUp = item.times;
+                    },item.delay);
+                    self._speedUpMap.push(a);
+                })(item);                
+            }
+
+        },
+        /**
+         * 停止加速
+         */
+        _stopSpeedUp : function(){
+            var self = this;
+            for(var i=0;i<self._speedUpMap.length;i++){
+                clearTimeout(self._speedUpMap[i]);
+            }
         },
         //主体的位置
         _mainPos : {},
@@ -241,26 +347,52 @@ KISSY.add(function (S, Node,Base) {
             mainBox.css({'left':'50%','margin-left':-cfg.width/2});
 
             self.Jnode.append(mainBox);
+            self._mainBox = mainBox;
 
             self._mainBoxWidth = cfg.width;
             self._mainBoxHeight = cfg.height;
             self._mainPos = {'top':cfg.top,'left':mainBox.offset().left};
 
-            self._moveMainBox(mainBox, cfg.top,mainBox.offset().left, cfg.direc);
+            self._moveMainBox(cfg.top,mainBox.offset().left, cfg.direc);
         },
         /**
          * 移动主体
          */
-        _moveMainBox : function(mainBox, top, left, direc){
-            var self = this;
+        _moveMainBox : function(top, left, direc){
+            var self = this,
+                mainBox = self._mainBox;
             var boxOffset = self.Jnode.offset(),
                 overLeft = parseInt(boxOffset.left),
                 overTop = parseInt(boxOffset.top) + mainBox.height()/2,
-                maxWidth = self._maxWidth;
+                maxWidth = self._maxWidth,
+                maxHeight = self._maxHeight,
+                mWidth = mainBox.width(),
+                mHeight = mainBox.height();
             
-            self.Jnode.on('mousemove',function(e){
+            $(document).on('mousemove',function(e){
+                e.preventDefault();
+                if(self._runStatus === false){
+                    self['clearInterval']();
+                    return;
+                }
                 var newLeft = e.clientX - overLeft,
                     newTop  = e.clientY - overTop;
+
+                //计算边界值
+                if(newLeft < 35 ){
+                    newLeft = 35;
+                }
+                if(newLeft + mWidth > maxWidth){
+                    newLeft = maxWidth - mWidth/2;
+                }
+
+                if(newTop < 0){
+                    newTop = 0;
+                }
+                if(newTop + mHeight > maxHeight){
+                    newTop = maxHeight - mHeight;
+                }
+
                 var changeCass = {};
                 switch(direc){
                     default:
@@ -277,9 +409,7 @@ KISSY.add(function (S, Node,Base) {
                         self._mainPos = {'top':newTop,'left':left};
                         break;
                 }
-                mainBox.css(changeCass);
-
-               
+                mainBox.css(changeCass);               
             });
         },
         /**
@@ -291,25 +421,50 @@ KISSY.add(function (S, Node,Base) {
                 mTop = self._mainPos.top,
                 mLeft = self._mainPos.left,
                 direc = self._direc,
-                overValue = 10,
+                overValue = 30,
                 mWidth = self._mainBoxWidth,
                 mHeight = self._mainBoxHeight;
 
-            var itemCallback = self.cfg.itemCallback;
-            var ml2 = mLeft - mWidth/2;
+            //游戏已经停止
+            if(self._runStatus === false){
+                self['clearInterval']();
+                return;
+            }           
 
             for(var i in self._fallObj){
                 var item = self._fallObj[i];
                 if(item == undf){
                     continue;
                 }
-                //if(mTop - 10 <= item.top && (mTop + boxHeight + 10) >= item.top){
-                //console.log(mLeft +'---'+item.left +'---'+mTop +'--'+item.top);
-                if(ml2 <= item.left && ml2 + mWidth >= item.left && mTop <= item.top && mTop + mHeight >= item.top){
+
+                if(mLeft - mWidth/2 - overValue <= item.left && mLeft - mWidth/2 + mWidth + overValue >= item.left && 
+                    mTop - overValue*2 <= item.top && mTop + mHeight + overValue >= item.top){
+
                     item.dom.remove();
+                    var type = item.type,
+                        left = item.left,
+                        top = item.top;
                     delete self._fallObj[i];
+                    if(self.cfg.itemCallback){
+                        self.cfg.itemCallback(self._mainBox, type, mLeft-mWidth/2, mTop-10);
+                    }
                 }
             }
+        },
+        /**
+         * 设置游戏定时器，定时扫描任务
+         */
+        setInterval : function(){
+            var self = this;
+            self._setInterval = setInterval(function(){
+                self._parseCollide();
+            },30); 
+        },
+        /**
+         * 停止游戏定时器
+         */
+        clearInterval : function(){
+            clearInterval(this._setInterval);
         },
         /**
          * 创建i到n的数组
